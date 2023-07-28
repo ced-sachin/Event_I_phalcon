cb86edd4a9dd3b16289a492d82b0909ddd7ce01f
Sachin Kumar <sachin05714@gmail.com>
2023-07-28 11:52:08 +0530

    adding roles

---
+++ src/app/controllers/AclsController.php
@@ -0,0 +1,21 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+use Phalcon\Forms\Form;
+use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Element\Select;
+
+
+class AclsController extends Controller
+{
+    public function indexAction()
+    {   
+        // die(__METHOD__);
+    }
+
+    public function rolesAction()
+    {   
+        
+    }
+
+}
